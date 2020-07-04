a3f566be207a016c5fa64084f4aa6c292c999ad9
Wills Blake <richwblake@gmail.com>
2020-07-04 14:33:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map (arr, callback) {
+    const newArr = [];
+    arr.forEach(element => newArr.push(callback(element)));
+    return newArr;
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
